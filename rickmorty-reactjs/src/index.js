e887d679588f2c3e8aa90118ac046b785d6eb23c
rumikoacopa <rumikorios@gmail.com>
2021-12-16 21:40:38 -0500

    installed cors and attempt to debug by adding cod to index.js but commented it out for time being

--- rickmorty-reactjs/src/index.js
+++ rickmorty-reactjs/src/index.js
@@ -4,13 +4,23 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+//*/const cors = require("cors");
+//const corsOptions = {
+//  origin: "http://localhost:3000",
+//  credentials: true, //access-control-allow-credentials:true
+//  optionSuccessStatus: 200
+//};
+//app.use(cors(corsOptions));
+//
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
+  
 );
 
+ 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
