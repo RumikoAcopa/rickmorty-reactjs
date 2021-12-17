25220cf67803ca831c285e1c156bca6ec77f02c1
rumikoacopa <rumikorios@gmail.com>
2021-12-16 19:38:59 -0500

    added import PostResidentList to App.js

--- rickmorty-reactjs/src/App.js
+++ rickmorty-reactjs/src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import PostResidentList from "./PostResidentList"
 
 function App() {
   return (
--- rickmorty-reactjs/src/PostResidentList.js
+++ rickmorty-reactjs/src/PostResidentList.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+
 class PostResidentList extends Component {
 
 
 
 
-  
+
 }
 
 export default PostResidentList;
